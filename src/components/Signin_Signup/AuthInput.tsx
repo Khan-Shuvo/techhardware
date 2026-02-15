07f4fce605dbeeb21a7e0c5771fd41359f375f65
khan-shuvo <shuvo2004@outlook.com>
2026-02-15 17:14:50 +0600

    received value and onchange method as props

--- src/components/Signin_Signup/AuthInput.tsx
+++ src/components/Signin_Signup/AuthInput.tsx
@@ -1,13 +1,20 @@
+"use client"
+import { SetStateAction } from "react"
+
 type Props = {
   label: string
+  value: string
   type?: string
   placeholder?: string
+  method?: React.Dispatch<SetStateAction<string>>
 }
 
 export default function AuthInput({
   label,
+  value,
   type = "text",
   placeholder,
+  method,
 }: Props) {
   return (
     <div className="flex flex-col gap-1">
@@ -16,6 +23,8 @@ export default function AuthInput({
       <input
         type={type}
         placeholder={placeholder}
+        value={value}
+        onChange={(e) => method?.(e.target.value) }
         className="
           border border-gray-300
           rounded-lg
